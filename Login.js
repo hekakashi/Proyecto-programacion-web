b9f9920a224ac422dc0a82079784010eea531f3d
vichoco21d <134302303+vichoco21d@users.noreply.github.com>
2024-05-09 12:03:59 -0400

    Newform javascript

--- Login.js
+++ Login.js
@@ -1,23 +1,39 @@
-const correo = document.getElementById("email")
-const password = document.getElementById("Password")
-const formulario = document.getElementById("formulario")
+document.addEventListener("DOMContentLoaded", function () {
+    const formulario = document.getElementById('formulario');
+    const mensajeError = document.getElementById('mensajeError');
 
-formulario.addEventListener("submit", e=>{
-    e.preventDefault()
-    let email = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/
-    let entrar = false
-    console.log(email.test(email.value))
-    if(email.test(email.value)){
-        alert("Correo no valido")
-        entrar = true
+    formulario.addEventListener('submit', function (evento) {
+        evento.preventDefault(); 
+        let email = document.getElementById('email').value;
+        let Password = document.getElementById('Password').value;
+
+        // Validación de los campos
+
+        if (email.trim() === '') {
+            mensajeError.innerText = 'El campo email no puede estar vacío.';
+            return;
+        }
+
+        if (!validarEmail(email)) {
+            mensajeError.innerText = 'El formato del email no es válido.';
+            return;
+        }
+
+        if(Password.length < 8){
+            mensajeError.innerText = 'Contraseña no valida(Minimo 8 caracteres).';
+            return;
+        }
 
 
+        // Si todo está correcto, se puede proceder a enviar el formulario o hacer alguna otra acción
+        mensajeError.innerText = '';
+        // alert('Formulario enviado con éxito!');
+        formulario.submit(); 
+    });
+
+    function validarEmail(email) {
+        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
     }
-    
-    if(password.value.length < 8){
-        alert("Contraseña no valida")
-        entrar = true
-    }
+});
 
-    
-})
