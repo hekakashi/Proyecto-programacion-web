5898d21e850b6c624f9704d5b36410d464e07098
helen <133412763+hekakashi@users.noreply.github.com>
2024-06-22 20:12:44 -0400

    mejora

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name='index')
+    path('carrito', views.carrito, name='carrito')
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.http import HTTResponse 
 
 # Create your views here.
 def index(request):
    context={}
-    return render(request, 'pages/index.html', context)
+   return render(request,'pages/index.html',context)
+
+def carrito(request):
+   context={}
+   return render (request, 'pages/carrito.html', context)
