47b2f995eb3832d9d0e126fcdb4fc0db1dc37601
helen <133412763+hekakashi@users.noreply.github.com>
2024-06-29 19:59:07 -0400

    cambio

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index,name='index'),
+    path('index',views.index,name='index'),
     path('carrito', views.carrito, name='carrito'),
+    path('Carrusel', views.carrusel, name='carrusel'),
+    path('hombre', views.hombre, name='hombre'),
+    path('carrito', views.hombre, name='hombre'),
+    path('Login', views.Login, name='Login'),
+    path('mujer', views.mujer, name='mujer'),
+    path('Regristo', views.Regristo, name='Regristo'),
+    
+    
 ]
