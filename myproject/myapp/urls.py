f07b51438d5ff1b49d00ed4e922f838302b765d2
helen <133412763+hekakashi@users.noreply.github.com>
2024-06-30 00:11:41 -0400

    quierojugarvalo:(

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name='index'),
-    path('carrito', views.carrito, name='carrito'),
-    path('carrusel', views.carrusel, name='carrusel'),
-    path('hombre', views.hombre, name='hombre'),
-    path('carrito', views.hombre, name='hombre'),
-    path('login', views.Login, name='Login'),
-    path('mujer', views.mujer, name='mujer'),
-    path('Regristo', views.Regristo, name='Regristo'),
+    path('carrito', views.carrito,name='carrito'),
+    path('carrusel', views.carrusel,name='carrusel'),
+    path('hombre', views.hombre,name='hombre'),
+    path('carrito', views.hombre,name='hombre'),
+    path('login', views.login,name='Login'),
+    path('mujer', views.mujer,name='mujer'),
+    path('regristo', views.registro,name='Regristo'),
 ]
