5ead7031d31508d276a6ddb80d05c8548a2d4e34
helen <133412763+hekakashi@users.noreply.github.com>
2024-06-29 23:15:40 -0400

    a

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -2,14 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index',views.index,name='index'),
+    path('',views.index,name='index'),
     path('carrito', views.carrito, name='carrito'),
-    path('Carrusel', views.carrusel, name='carrusel'),
+    path('carrusel', views.carrusel, name='carrusel'),
     path('hombre', views.hombre, name='hombre'),
     path('carrito', views.hombre, name='hombre'),
-    path('Login', views.Login, name='Login'),
+    path('login', views.Login, name='Login'),
     path('mujer', views.mujer, name='mujer'),
     path('Regristo', views.Regristo, name='Regristo'),
-    
-    
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -2,9 +2,25 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-   context={}
+   context = {}
    return render(request,'pages/index.html',context)
 
 def carrito(request):
-   context={}
+   context = {}
    return render (request, 'pages/carrito.html', context)
+
+def hombre(request):
+   context = {}
+   return render (request, 'pages/hombre.html' ,context )
+
+def login(request):
+   context = {}
+   return render (request, 'pages/login', context)
+
+def mujer(request):
+   context = {}
+   return render (request, 'pages/mujer', context)
+
+def registro(request):
+   context = {}
+   return render(request, 'pages/registro', context)
